816bb7fe8e2b5e7d09469863a3c10125d7a0a1be
luanhosi <luanho192@gmail.com>
2024-06-12 22:42:13 +0700

    add find index of first occurence

---
+++ FindIndexOfFirstOccurence/main.swift
@@ -0,0 +1,25 @@
+import UIKit
+//Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//
+// 
+//
+//Example 1:
+//
+//Input: haystack = "sadbutsad", needle = "sad"
+//Output: 0
+//Explanation: "sad" occurs at index 0 and 6.
+//The first occurrence is at index 0, so we return 0.
+//Example 2:
+//
+//Input: haystack = "leetcode", needle = "leeto"
+//Output: -1
+//Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+
+func strStr(_ haystack: String, _ needle: String) -> Int {
+    if let range = haystack.range(of: needle) {
+        let startIndex = haystack.distance(from: haystack.startIndex, to: range.lowerBound)
+        return startIndex
+    }
+    return -1
+}
