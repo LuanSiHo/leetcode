db5398000fbe2cfbfccdd6718e3b82ae19b9b45e
luanhosi <luanho192@gmail.com>
2024-06-12 22:55:11 +0700

    add move zeroes

---
+++ MoveZeroes/main.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+//Note that you must do this in-place without making a copy of the array.
+//
+// 
+//
+//Example 1:
+//
+//Input: nums = [0,1,0,3,12]
+//Output: [1,3,12,0,0]
+//Example 2:
+//
+//Input: nums = [0]
+//Output: [0]
+// 
+//
+//Constraints:
+//
+//1 <= nums.length <= 104
+//-231 <= nums[i] <= 231 - 1
+// 
+//
+//Follow up: Could you minimize the total number of operations done?
+
+
+func moveZeroes(_ nums: inout [Int]) {
+    var numberOfZero = 0
+    for (index, value) in nums.enumerated() {
+        if value == 0 {
+            numberOfZero = numberOfZero + 1
+        } else if numberOfZero > 0 {
+            var temp = value
+            nums[index] = 0
+            nums[index - numberOfZero] = temp
+        }
+    }
+}
